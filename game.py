519be13d9ef5b6b4c3ef7415cfa0a4e9dce834f6
Hubert Jaczyński <hubert.jaczynski@gmail.com>
2024-12-22 01:20:56 +0100

    part 2 of tutorial

--- game.py
+++ game.py
@@ -64,7 +64,7 @@ class SnakeGame:
         self.snake.insert(0, self.head)
         reward = 0
         game_over = False
-        if self._is_collision() or self.frame_iteration > 100*len(self.snake):
+        if self.is_collision() or self.frame_iteration > 100*len(self.snake):
             game_over = True
             reward = -10
             return game_over, self.score
@@ -80,7 +80,7 @@ class SnakeGame:
         self.clock.tick(SPEED)
         return reward, game_over, self.score
 
-    def _is_collision(self, point=None):
+    def is_collision(self, point=None):
         if point is None:
             point = self.head
         if point.x > self.width or point.x < 0 or point.y > self.height or point.y < 0:
